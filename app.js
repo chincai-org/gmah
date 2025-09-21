f2c82d86976bd5f5190757e00dcbac5d2264b14e
CEOofYap <johncenavstherock0126@gmail.com>
2025-09-21 17:42:02 +0800

    feat: grammar ejs

--- app.js
+++ app.js
@@ -461,6 +461,85 @@ app.post("/generate-vocab-lesson", cookieAuth, async (req, res) => {
 
 app.post("/generate-dialogue-lesson", cookieAuth, async (req, res) => {});
 
+app.get("/grammar", (req, res) => {
+    const topic = {
+        //get from db
+        topicId: 12345,
+        title: "topicTitle",
+        content:
+            "This is topic content that teaches about blah blah blah in markdown",
+        description: "Placeholder description",
+        items: [
+            //generate grammar questions
+            {
+                question: "1. First question is zh gay?",
+                options: [
+                    {
+                        text: "Yes definitely.",
+                        correct: true
+                    },
+                    {
+                        text: "Yes 2 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 3 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 4 definitely.",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "2. second question is zh clanker?",
+                options: [
+                    {
+                        text: "Yes definitely.",
+                        correct: true
+                    },
+                    {
+                        text: "Yes 2 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 3 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 4 definitely.",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "3. second question is zh short?",
+                options: [
+                    {
+                        text: "Yes definitely.",
+                        correct: true
+                    },
+                    {
+                        text: "Yes 2 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 3 definitely.",
+                        correct: false
+                    },
+                    {
+                        text: "Yes 4 definitely.",
+                        correct: false
+                    }
+                ]
+            }
+        ]
+    };
+
+    res.render("grammar", { topic });
+});
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
